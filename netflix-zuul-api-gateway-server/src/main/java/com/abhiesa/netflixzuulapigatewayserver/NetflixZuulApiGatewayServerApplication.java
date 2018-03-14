0f705454b2293f84492f09bebbc9643102078f02
Abhishek Pandey <pandeyab@HCL.COM>
2018-03-14 16:40:50 +0530

    initial commit

---
+++ netflix-zuul-api-gateway-server/src/main/java/com/abhiesa/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplication.java
@@ -0,0 +1,12 @@
+package com.abhiesa.netflixzuulapigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixZuulApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixZuulApiGatewayServerApplication.class, args);
+	}
+}

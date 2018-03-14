1b70ec61db02f2a11d16f483180ebe7738d93792
Abhishek Pandey <pandeyab@HCL.COM>
2018-03-14 17:01:07 +0530

    added swagger

--- currency-conversion-service/src/main/java/com/abhiesa/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/abhiesa/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -7,9 +7,16 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @EnableFeignClients("com.abhiesa.currencyconversionservice")
 @EnableDiscoveryClient
+@EnableSwagger2
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
@@ -21,4 +28,12 @@ public class CurrencyConversionServiceApplication {
 		return new AlwaysSampler();
 	}
 
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.any())                          
+          .build();                                           
+    }
 }
--- currency-exchange-service/src/main/java/com/abhiesa/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ currency-exchange-service/src/main/java/com/abhiesa/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -6,8 +6,15 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableSwagger2
 public class CurrencyExchangeServiceApplication {
 
 	public static void main(String[] args) {
@@ -18,5 +25,14 @@ public class CurrencyExchangeServiceApplication {
 	public AlwaysSampler defaultSampler(){ 
 	    return new AlwaysSampler(); 
 	} 
+	
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.any())                          
+          .build();                                           
+    }
 
 }
